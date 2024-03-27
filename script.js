b2cde7be271cfc04bc41a14859ee9f35b166fab5
int3rrobang <80174663+int3rrobang@users.noreply.github.com>
2024-03-27 16:58:46 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const themeSwitch = document.getElementById('theme-switch');
-const body = document.body;
-
-themeSwitch.addEventListener('change', function () {
-  body.classList.toggle('dark-mode');
-});
